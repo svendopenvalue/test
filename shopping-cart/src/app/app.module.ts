37398af10edf999aca4c692f23b7e206ac17f6ec
Svend Therkildsen <svend.therkildsen@openvalue.fr>
2021-05-27 10:45:21 +0200

    components and service OK

--- shopping-cart/src/app/app.module.ts
+++ shopping-cart/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
+import { AddToListComponent } from './add-to-list/add-to-list.component';
+import { ListComponent } from './list/list.component';
+import { TotalComponent } from './total/total.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentComponent
+    ContentComponent,
+    AddToListComponent,
+    ListComponent,
+    TotalComponent
   ],
   imports: [
     BrowserModule,
